27713a2f80b2ce7d4058c4558c9116ada08daf20
hazynatulasrori <56811379+hazynatulasrori@users.noreply.github.com>
2019-10-21 23:10:12 +0700

    Add files via upload

---
+++ application/views/PP.php
@@ -0,0 +1,23 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body align="center">
+    <h1>WES LUWEH</h1>    
+    <?php
+    $nama = "RORI";
+    $title = "Tugas2";
+    ?>
+
+    <p>nama : <?php echo $nama?></p>
+    <p>title : <?php echo $title?></p>
+
+</body>
+</html>
